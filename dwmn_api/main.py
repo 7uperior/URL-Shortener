65e26d10c7b6c54a6b72bc70ba083435914c9e92
Vadim Surin <7uperior@gmail.com>
2023-12-26 11:11:58 +0300

    main.py has been dried

--- dwmn_api/main.py
+++ dwmn_api/main.py
@@ -5,20 +5,16 @@ from urllib.parse import urlparse
 
 def shorten_link(url, token_api):
     headers = {"Authorization": f"Bearer {token_api}"}
-    bitly_new_link_endpoint = "https://api-ssl.bitly.com/v4/bitlinks"
-    response = requests.post(
-        bitly_new_link_endpoint, headers=headers, json={"long_url": url}
-    )
+    endpoint = "https://api-ssl.bitly.com/v4/bitlinks"
+    response = requests.post(endpoint, headers=headers, json={"long_url": url})
     response.raise_for_status()
     return response.json()["id"]
 
 
 def count_the_number_of_clicks_on_bitly_link(url, token_api):
     headers = {"Authorization": f"Bearer {token_api}"}
-    bitly_summary_endpoint = (
-        f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary"
-    )
-    response = requests.get(bitly_summary_endpoint, headers=headers)
+    endpoint = f"https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary"
+    response = requests.get(endpoint, headers=headers)
     response.raise_for_status()
     return response.json()["total_clicks"]
 
@@ -27,8 +23,8 @@ def is_bitlink(url, token_api):
     headers = {"Authorization": f"Bearer {token_api}"}
     parsed_url = urlparse(url)
     bitlink = f"{parsed_url.netloc}{parsed_url.path}"
-    bitly_check_endpoint = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
-    response = requests.get(bitly_check_endpoint, headers=headers)
+    endpoint = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
+    response = requests.get(endpoint, headers=headers)
     if response.status_code == 200:
         return True
 
